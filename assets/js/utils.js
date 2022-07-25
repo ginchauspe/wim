70bd4f8da3964e1d8a602509b5ebf8c0c16e5d92
ginchauspe <ginchauspe@gmail.com>
2022-07-25 10:51:56 -0300

    Started to add Plotly charts.

---
+++ assets/js/utils.js
@@ -0,0 +1,5 @@
+export const RED_SHADES = ['#cf0821', '#f43d3a', '#ff6055', '#ff8171', '#ffa28e'];
+export const GREEN_SHADES = ['#009560', '#007846', '#005c2d', '#004216', '#002a00'];
+
+export const BOKE_NAMES = ['Tanene', 'Sangarédi', 'Kanfarande', 'Kamsar', 'Koulabougni'];
+export const BOFFA_NAMES = ['Tougnifily', 'Doupourou', 'Tamita', 'Colia (Wakili)', 'Colia (Yètèmaly)'];
